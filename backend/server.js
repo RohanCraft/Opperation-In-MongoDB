a2b6dadbe4758f9d6413a7d283203a275827241e
RohanCraft <rohankumar9969@gmail.com>
2025-04-26 19:53:45 +0530

    correct the studentRoute path

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import StudentRoutes from "./routes/studentRoutes.js";
+import StudentRoutes from "./routes/StudentRoutes.js";
 
 
 dotenv.config();
